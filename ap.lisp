2ff58adce234b573d7dcb10aa105ef3c9546ea7d
Martin Kielhorn <kielhorn.martin@gmail.com>
2017-05-10 21:25:51 +0200

    too many semicolons

--- ap.lisp
+++ ap.lisp
@@ -662,6 +662,10 @@ end;
 		      (function (Empty ((Queue Queue_Type :i)) Boolean))
 		      (function (Size ((Queue Queue_Type :i)) Natural))
 		      (function (First_Element ((Queue Queue_Type :i)) Element_Type ((with (=> (Pre (not (call Empty Queue))))))))
+		      (function (Last_Element ((Queue Queue_Type :i)) Element_Type
+					      ((with (=> (Pre (not (call Empty Queue))))))))
+		      (function (Clear ((Queue Queue_Type :i)) Element_Type
+					      ((with (=> (Post (and-then (call Empty Queue) (= (call Size Queue) 0))))))))
 
 		      (procedure (Enqueue ((Queue Queue_Type :io)
 					   (Item Element_Type :i))
@@ -669,12 +673,46 @@ end;
 						     (Post  (and-then (not (call Empty Queue))
 								      (= (call Size Queue)
 									 (call Size (+ (attrib Queue Old) 1)))
-								      (= (call Last_Element Queue) Item)))))
-					   (decl ((A Integer)))))
-				 (call New_Line)))))
-  (emit-ada :code code)
+								      (= (call Last_Element Queue) Item))))))))
+		      (procedure (Dequeue ((Queue Queue_Type :io)
+					   (Item Element_Type :i))
+					  ((with (=> (Pre (not (call Empty Queue)))
+						     (Post  (and-then (= Item (call First_Element (attrib Queue Old)))
+								      (= (call Size Queue)
+									 (- (call Size (attrib Queue Old)) 1))))))))))))
   
-  (write-source "Bounded_Queue_V1" "ads" code))
+
+
+
+  
+  (write-source "Bounded_Queue_V1" "ads" code)
+    (emit-ada :code code))
+
+#+nil
+"package Bounded_Queue_V1 is
+  subtype Element_Type is Integer;
+  type Queue_Array is array ( Positive range <> ) of Element_Type;
+  type Queue_Type(MaxSize : in Positive) is record
+  Count : Natural ;
+  Front : Positive ;
+  Rear : Positive ;
+  Items : Queue_Array(1 .. Max_Size) ;
+end record;
+  function Full (Queue : in Queue_Type) return Boolean;
+  function Empty (Queue : in Queue_Type) return Boolean;
+  function Size (Queue : in Queue_Type) return Natural;
+  function First_Element (Queue : in Queue_Type) return Element_Type  with (Pre => (not (Empty(Queue))));
+;
+  function Last_Element (Queue : in Queue_Type) return Element_Type  with (Pre => (not (Empty(Queue))));
+;
+  function Clear (Queue : in Queue_Type) return Element_Type  with (Post => Empty(Queue) and then Size(Queue) = 0);
+;
+  procedure Enqueue (Queue : in out Queue_Type; Item : in Element_Type)  with (Pre => (not (Full(Queue))), Post => (not (Empty(Queue))) and then Size(Queue) = Size(Queue'Old + 1) and then Last_Element(Queue) = Item);
+;
+  procedure Dequeue (Queue : in out Queue_Type; Item : in Element_Type)  with (Pre => (not (Empty(Queue))), Post => Item = First_Element(Queue'Old) and then Size(Queue) = Size(Queue'Old) - 1);
+;
+end Bounded_Queue_V1;"
+
 #+nil
 "procedure Enqueue (Queue : in out Queue_Type; Item : in Element_Type) is
   with (Pre => (not (Full(Queue))), Post => (not (Empty(Queue))) and then Size(Queue) = Size(Queue'Old + 1) and then Last_Element(Queue) = Item);
