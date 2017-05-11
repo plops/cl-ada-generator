c322b167da6646bd177efc5c2b9d1186a37e4a46
Martin Kielhorn <kielhorn.martin@gmail.com>
2017-05-11 08:06:07 +0200

    too many semicolon
    error in record (no in)
    remove parens around (Pre => ...)
    
    package Bounded_Queue_V1 is
       subtype Element_Type is Integer;
       type Queue_Array is array (Positive range <>) of Element_Type;
       type Queue_Type (Max_Size : Positive) is record
          Count : Natural;
          Front : Positive;
          Rear  : Positive;
          Items : Queue_Array (1 .. Max_Size);
       end record;
       function Full (Queue : in Queue_Type) return Boolean;
       function Empty (Queue : in Queue_Type) return Boolean;
       function Size (Queue : in Queue_Type) return Natural;
       function First_Element (Queue : in Queue_Type) return Element_Type with
          Pre => not Empty (Queue);
    
       function Last_Element (Queue : in Queue_Type) return Element_Type with
          Pre => (not (Empty (Queue)));
    
       function Clear (Queue : in Queue_Type) return Element_Type with
          Post => Empty (Queue) and then Size (Queue) = 0;
    
          -- procedure Enqueue (Queue : in out Queue_Type; Item : in Element_Type)  with Pre => (not (Full(Queue))), Post => (not (Empty(Queue))) and then Size(Queue) = Size(Queue'Old + 1) and then Last_Element(Queue) = Item;
    
       procedure Dequeue (Queue : in out Queue_Type; Item : in Element_Type) with
          Pre  => (not (Empty (Queue))),
          Post => Item = First_Element (Queue'Old)
          and then Size (Queue) = Size (Queue'Old) - 1;
    
    end Bounded_Queue_V1;

--- ap.lisp
+++ ap.lisp
@@ -675,61 +675,61 @@ end;
 
 
 (let ((def `(package Bounded_Queue_V1
-		      (subtype Element_Type Integer)
-		      (type Queue_Array (array ((range nil nil :type Positive)) Element_Type))
-		      (type (discriminant Queue_Type ((MaxSize Positive)))
-			    (record ((Count Natural)
-				     (Front Positive)
-				     (Rear Positive)
-				     (Items (aref Queue_Array (dots 1 Max_Size))))))
-		      (function (Full ((Queue Queue_Type :i)) Boolean))
+		     (subtype Element_Type Integer)
+		     (type Queue_Array (array ((range nil nil :type Positive)) Element_Type))
+		     (type (discriminant Queue_Type ((MaxSize Positive)))
+			   (record ((Count Natural)
+				    (Front Positive)
+				    (Rear Positive)
+				    (Items (aref Queue_Array (dots 1 Max_Size))))))
+		     (function (Full ((Queue Queue_Type :i)) Boolean))
 
-		      (function (Empty ((Queue Queue_Type :i)) Boolean))
-		      (function (Size ((Queue Queue_Type :i)) Natural))
-		      (function (First_Element ((Queue Queue_Type :i)) Element_Type ((with (=> (Pre (not (call Empty Queue))))))))
-		      (function (Last_Element ((Queue Queue_Type :i)) Element_Type
-					      ((with (=> (Pre (not (call Empty Queue))))))))
-		      (function (Clear ((Queue Queue_Type :i)) Element_Type
-					      ((with (=> (Post (and-then (call Empty Queue) (= (call Size Queue) 0))))))))
+		     (function (Empty ((Queue Queue_Type :i)) Boolean))
+		     (function (Size ((Queue Queue_Type :i)) Natural))
+		     (function (First_Element ((Queue Queue_Type :i)) Element_Type ((with (=> (Pre (not (call Empty Queue))))))))
+		     (function (Last_Element ((Queue Queue_Type :i)) Element_Type
+					     ((with (=> (Pre (not (call Empty Queue))))))))
+		     (function (Clear ((Queue Queue_Type :i)) Element_Type
+				      ((with (=> (Post (and-then (call Empty Queue) (= (call Size Queue) 0))))))))
 
-		      (procedure (Enqueue ((Queue Queue_Type :io)
-					   (Item Element_Type :i))
-					  ((with (=> (Pre (not (call Full Queue)))
-						     (Post  (and-then (not (call Empty Queue))
-								      (= (call Size Queue)
-									 (call Size (+ (attrib Queue Old) 1)))
-								      (= (call Last_Element Queue) Item))))))))
-		      (procedure (Dequeue ((Queue Queue_Type :io)
-					   (Item Element_Type :i))
-					  ((with (=> (Pre (not (call Empty Queue)))
-						     (Post  (and-then (= Item (call First_Element (attrib Queue Old)))
-								      (= (call Size Queue)
-									 (- (call Size (attrib Queue Old)) 1)))))))))))
+		     (procedure (Enqueue ((Queue Queue_Type :io)
+					  (Item Element_Type :i))
+					 ((with (=> (Pre (not (call Full Queue)))
+						    (Post  (and-then (not (call Empty Queue))
+								     (= (call Size Queue)
+									(call Size (+ (attrib Queue Old) 1)))
+								     (= (call Last_Element Queue) Item))))))))
+		     (procedure (Dequeue ((Queue Queue_Type :io)
+					  (Item Element_Type :i))
+					 ((with (=> (Pre (not (call Empty Queue)))
+						    (Post  (and-then (= Item (call First_Element (attrib Queue Old)))
+								     (= (call Size Queue)
+									(- (call Size (attrib Queue Old)) 1)))))))))))
       (code `(package-body Bounded_Queue_V1
-		      (function (Full ((Queue Queue_Type :i)) Boolean)
-				(return (= Queue.Count Queue.Max_Size)))
-		      (function (Empty ((Queue Queue_Type :i)) Boolean)
-				(return (= Queue.Count 0)))
-		      (function (Size ((Queue Queue_Type :i)) Natural)
-				(return Queue.Count))
-		      (function (First_Element ((Queue Queue_Type :i)) Element_Type)
-				(return (aref Queue.Items Queue.Front)))
-		      (function (Last_Element ((Queue Queue_Type :i)) Element_Type)
-				(return (aref Queue.Items Queue.Rear)))
-		      (procedure (Clear ((Queue Queue_Type :io)))
-				 (setf Queue.Count 0
-				       Queue.Front 1
-				       Queue.Rear Queue.Max_Size))
-		      (procedure (Enqueue ((Queue Queue_Type :io)
-					   (Item Element_Type :i)))
-				 (setf Queue.Rear (rem Queue.Rear (+ Queue.Max_Size 1))
-				       (aref Queue.Items Queue.Rear) Item)
-				 (incf Queue.Count))
-		      (procedure (Dequeue ((Queue Queue_Type :io)
-					   (Item Element_Type :i)))
-				 (setf Item (aref Queue.Items Queue.Front)
-				       Queue.Front (rem Queue.Front (+ Queue.Max_Size 1)))
-				 (decf Queue.Count))))
+			   (function (Full ((Queue Queue_Type :i)) Boolean)
+				     (return (= Queue.Count Queue.Max_Size)))
+			   (function (Empty ((Queue Queue_Type :i)) Boolean)
+				     (return (= Queue.Count 0)))
+			   (function (Size ((Queue Queue_Type :i)) Natural)
+				     (return Queue.Count))
+			   (function (First_Element ((Queue Queue_Type :i)) Element_Type)
+				     (return (aref Queue.Items Queue.Front)))
+			   (function (Last_Element ((Queue Queue_Type :i)) Element_Type)
+				     (return (aref Queue.Items Queue.Rear)))
+			   (procedure (Clear ((Queue Queue_Type :io)))
+				      (setf Queue.Count 0
+					    Queue.Front 1
+					    Queue.Rear Queue.Max_Size))
+			   (procedure (Enqueue ((Queue Queue_Type :io)
+						(Item Element_Type :i)))
+				      (setf Queue.Rear (rem Queue.Rear (+ Queue.Max_Size 1))
+					    (aref Queue.Items Queue.Rear) Item)
+				      (incf Queue.Count))
+			   (procedure (Dequeue ((Queue Queue_Type :io)
+						(Item Element_Type :i)))
+				      (setf Item (aref Queue.Items Queue.Front)
+					    Queue.Front (rem Queue.Front (+ Queue.Max_Size 1)))
+				      (decf Queue.Count))))
       (call `(with-compilation-unit
 		 (with-use Bounded_Queue_V1)
 	       (with-use Ada.Text_IO)
@@ -745,10 +745,11 @@ end;
 			  (setf Value (call Size My_Queue))
 			  (call Put_Line (& (string "Size of cleared queue is ")
 					    (attrib Integer (call Image Value))))))))
-  (write-source "Bounded_Queue_V1" "ads" def)
-  (write-source "Bounded_Queue_V1" "adb" code)
-  (write-source "Bounded_Queue_V1_call" "adb" call)
-  (emit-ada :code code))
+  (write-source #P"/dev/shm/q/" "bounded_queue_v1" "ads" def)
+  (write-source #P"/dev/shm/q/" "bounded_queue_v1" "adb" code)
+  (write-source #P"/dev/shm/q/" "bounded_queue_v1_call" "adb" call)
+  ;(emit-ada :code code)
+  )
 
 #+nil
 "package Bounded_Queue_V1 is
@@ -785,6 +786,7 @@ begin
   New_Line;
 end;
 "
+#+nil
 (progn
   (with-open-file (s "o.adb" :direction :output :if-exists :supersede)
    (emit-ada :str s :code `(with-compilation-unit ;; procedure in second level
@@ -806,9 +808,9 @@ end;
 
 (defparameter *file-hashes* (make-hash-table))
 
-(defun write-source (name extension code)
+(defun write-source (folder name extension code)
   (let* ((fn (merge-pathnames (format nil "~a.~a" name extension)
-                              (user-homedir-pathname)))
+                              folder #+nil (user-homedir-pathname)))
          (code-str (emit-ada
                     :clear-env t
                     :code code))
